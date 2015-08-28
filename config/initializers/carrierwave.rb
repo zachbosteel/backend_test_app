83ee37aae9a699b9a5a720a41bcd48e3ddd9bd3f
Zach Bosteel <zbosteel@gmail.com>
2015-08-28 14:24:43 -0500

    reverting to Rails root for demo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET']
-    :endpoint              => "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
+    # :endpoint              => "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
     }
     config.fog_directory  = ENV['S3_BUCKET']
     config.fog_public     = false
-    config.root           = "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
-    config.cache_dir      = "tmp/uploads"
+    # config.root           = "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
+    config.cache_dir      = "#{Rails.root}/tmp/uploads"
   end
