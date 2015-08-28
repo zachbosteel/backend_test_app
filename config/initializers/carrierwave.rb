4efbe980bb33b7c7796e291d3ab654a559a4b8fb
Zach Bosteel <zbosteel@gmail.com>
2015-08-27 20:50:36 -0500

    trying process in background

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,7 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET']
+    :endpoint              => "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
     }
     config.fog_directory  = ENV['S3_BUCKET']
     config.fog_public     = false
