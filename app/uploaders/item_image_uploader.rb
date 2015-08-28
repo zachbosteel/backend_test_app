db3784b87da506e544a44283b226c2cdd57208d8
Zach Bosteel <zbosteel@gmail.com>
2015-08-28 15:30:12 -0500

    working locally

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -11,9 +11,9 @@ class ItemImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
+  # def cache_dir
+  #   "#{Rails.root}/tmp/uploads"
+  # end
 
   def auto_orient
     manipulate! do |img|
